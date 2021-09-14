f7866fb81b2b4c30001c28a0038794d5e6520a49
jinsoo <admin@binary01.me>
2021-09-14 16:44:09 +0900

    feat: end

--- index.js
+++ index.js
@@ -1,31 +1,91 @@
 const aws = require("aws-sdk");
 const cloudfront = new aws.CloudFront();
+const codepipeline = new aws.CodePipeline();
 
-exports.handler = async (event) => {
+exports.handler = async (event, context) => {
   // TODO implement
   console.log(
     event["CodePipeline.job"]["data"]["actionConfiguration"]["configuration"]
   );
+
+  const jobId = event["CodePipeline.job"].id;
+
   const userParams =
     event["CodePipeline.job"]["data"]["actionConfiguration"]["configuration"][
       "UserParameters"
     ];
 
-  console.log(userParams["distributionId"], userParams["objectPaths"]);
+  const parsedParams = JSON.parse(userParams);
 
   const params = {
-    DistributionId: userParams["distributionId"] /* required */,
+    DistributionId: parsedParams["distributionId"] /* required */,
     InvalidationBatch: {
-      CallerReference: "" + new Date().getTime() /* required */,
+      CallerReference: `Invalidation-LAMBDA-${new Date().getTime()}` /* required */,
       Paths: {
-        Quantity: userParams["objectPaths"].length /* required */,
-        Items: userParams["objectPaths"],
+        Quantity: 1 /* required */,
+        Items: ["/*"],
       },
     },
   };
-  cloudfront.createInvalidation(params, (err, data) => {
-    if (err) console.log(err, err.stack);
-    else console.log(data); // successful response
-  });
-  return null;
+
+  const putJobSuccess = async (message) => {
+    const params = {
+      jobId: jobId,
+    };
+    await codepipeline
+      .putJobSuccessResult(params)
+      .promise()
+      .then(async () => {
+        await context.succeed(message);
+      })
+      .catch(async (err) => {
+        await context.fail(err);
+        throw err;
+      });
+  };
+
+  // Notify CodePipeline of a failed job
+  const putJobFailure = async (message) => {
+    const params = {
+      jobId: jobId,
+      failureDetails: {
+        message: JSON.stringify(message),
+        type: "JobFailed",
+        externalExecutionId: context.awsRequestId,
+      },
+    };
+    await codepipeline
+      .putJobFailureResult(params)
+      .promise()
+      .then(async (err, data) => {
+        await context.fail(message);
+        throw err;
+      });
+  };
+
+  console.log(parsedParams["distributionId"], parsedParams["objectPaths"]);
+
+  if (!parsedParams["distributionId"] || !parsedParams["objectPaths"]) {
+    await putJobFailure("distributionId not Exist");
+    const response = {
+      statusCode: 400,
+    };
+    return response;
+  }
+
+  await cloudfront
+    .createInvalidation(params)
+    .promise()
+    .then(async () => {
+      await putJobSuccess("Invalidation Succes");
+    })
+    .catch(async (err) => {
+      await putJobFailure("Error for Invalidation");
+      throw err;
+    });
+
+  const response = {
+    statusCode: 200,
+  };
+  return response;
 };
